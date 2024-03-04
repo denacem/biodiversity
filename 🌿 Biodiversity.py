836fbd5d4da58ab7277924d70383187e010f18e4
Mirco Blaser <denacem@gmail.com>
2024-03-04 11:04:10 +0100

    Calculate PDF for world and swiss trade

--- 🌿 Biodiversity.py
+++ 🌿 Biodiversity.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import locale
 import numpy as np
+import re
 
 from crop_selector import select_crop
 
@@ -12,10 +13,27 @@ st.set_page_config(layout="wide")
 
 st.title("🌿 Biodiversity")
 
+#pd.set_option('display.float_format', '{:.2g}'.format)
+
 selected_crop, crop_type, impex_world_data, impex_swiss_data, yield_data = select_crop()
 
-# Load the countries lookup table
+# Load the lookup tables
 countries_data = pd.read_csv("./data/countries.csv")
+pdf_data = pd.read_csv("./data/pdf.csv")
+crops_data = pd.read_csv("./data/crops.csv")
+
+# Extract the 'occupation' based on the 'crop' from selected_crop
+occupation = crops_data.loc[crops_data['crop'] == selected_crop, 'occupation'].iloc[0]
+
+# Custom functions for Swiss number formatting
+
+def format_swiss_number(number):
+    formatted_number = locale.format("%0.2f", number, grouping=True)
+    return formatted_number.replace(locale.localeconv()['decimal_point'], '.')
+
+def format_swiss_number_sci(number):
+    formatted_number = "{:.2e}".format(number)
+    return formatted_number.replace(locale.localeconv()['decimal_point'], '.')
 
 # Filter only import and export data
 import_world_data = impex_world_data[impex_world_data['Element'] == 'Import Quantity'].set_index('Area')['Value']
@@ -78,24 +96,36 @@ result_world_data = result_world_data.fillna(0)
 # Calculate the 'Weighted (m^2/kg)' column
 result_world_data['Weighted (m^2/kg)'] = (result_world_data['Yield (m^2/kg)'] * result_world_data['Share (%)']) / total_share
 
+split_columns = pdf_data['occupation/transformation'].str.split(',', n=3, expand=True)
+
+pdf_data['occupation'] = split_columns[1].str.strip()
+pdf_data['country_code'] = split_columns[2].str.strip()
+
+# Now, you can use these two columns for merging
+result_world_data_merged = pd.merge(result_world_data, pdf_data, left_on='short', right_on='country_code', how='left')
+
+# Fill NaN values in 'amount' with 0
+result_world_data['temporary pdf value'] = result_world_data_merged['amount'].fillna(0)
+result_world_data['temporary pdf value'] = result_world_data['temporary pdf value']
+
+#st.write(type(result_world_data['temporary pdf value'][0]))
+
+result_world_data['PDF'] = result_world_data['temporary pdf value'] * result_world_data['Weighted (m^2/kg)']
+
 # Rename columns
 result_world_data = result_world_data.rename(columns={'Area': 'Trade Partner', 'short': 'Code', 'Value_x': 'Import (t)', 'Value_y': 'Yield (m^2/kg)'})
 
 # Reorder the columns
-result_world_data = result_world_data[['Trade Partner', 'Area Code (M49)', 'Code', 'Import (t)', 'Export (t)', 'Net (t)', 'Yield (m^2/kg)', 'Share (%)', 'Weighted (m^2/kg)']]
-
-# Custom function for Swiss number formatting
-def format_swiss_number(number):
-    formatted_number = locale.format("%0.4f", number, grouping=True)
-    return formatted_number.replace(locale.localeconv()['decimal_point'], '.')
+result_world_data = result_world_data[['Trade Partner', 'Code', 'Import (t)', 'Export (t)', 'Net (t)', 'Yield (m^2/kg)', 'Share (%)', 'Weighted (m^2/kg)', 'PDF']]
 
 # Apply the custom formatting function
 result_world_data['Import (t)'] = result_world_data['Import (t)'].apply(format_swiss_number)
 result_world_data['Export (t)'] = result_world_data['Export (t)'].apply(format_swiss_number)
 result_world_data['Net (t)'] = result_world_data['Net (t)'].apply(format_swiss_number)
-result_world_data['Yield (m^2/kg)'] = result_world_data['Yield (m^2/kg)'].apply(format_swiss_number)
+result_world_data['Yield (m^2/kg)'] = result_world_data['Yield (m^2/kg)'].apply(format_swiss_number_sci)
 result_world_data['Share (%)'] = result_world_data['Share (%)'].apply(format_swiss_number)
-result_world_data['Weighted (m^2/kg)'] = result_world_data['Weighted (m^2/kg)'].apply(format_swiss_number)
+result_world_data['Weighted (m^2/kg)'] = result_world_data['Weighted (m^2/kg)'].apply(format_swiss_number_sci)
+result_world_data['PDF'] = result_world_data['PDF'].apply(format_swiss_number_sci)
 
 # Show the result
 st.subheader(f"Biodiversity Calculation (World Trade) for {selected_crop.title()}")
@@ -165,26 +195,29 @@ impex_swiss_data = impex_swiss_data.fillna(0)
 # Calculate the 'Weighted (m^2/kg)' column
 impex_swiss_data['Weighted (m^2/kg)'] = (impex_swiss_data['Yield (m^2/kg)'] * impex_swiss_data['Share (%)']) / total_share
 
-# Rename columns
-#impex_swiss_data = impex_swiss_data.rename(columns={'Value_x': 'Import (t)', 'Value_y': 'Yield (m^2/kg)'})
+# Merge with countries lookup table to get short codes
+impex_swiss_data = pd.merge(impex_swiss_data, countries_data[['Name', 'short']], left_on='Trade Partner', right_on='Name', how='left')
 
-#st.write(impex_swiss_data)
+# Now, you can use these two columns for merging
+impex_swiss_data_merged = pd.merge(impex_swiss_data, pdf_data, left_on='short', right_on='country_code', how='left')
+
+# Fill NaN values in 'amount' with 0
+impex_swiss_data['temporary pdf value'] = impex_swiss_data_merged['amount'].fillna(0)
+impex_swiss_data['temporary pdf value'] = impex_swiss_data['temporary pdf value']
+
+impex_swiss_data['PDF'] = impex_swiss_data['temporary pdf value'] * impex_swiss_data['Weighted (m^2/kg)']
 
 # Reorder the columns
-impex_swiss_data = impex_swiss_data[['Trade Partner', 'Import Quantity (kg)', 'Export Quantity (kg)', 'Net (kg)', 'Yield (m^2/kg)', 'Share (%)', 'Weighted (m^2/kg)']]
-
-# Custom function for Swiss number formatting
-def format_swiss_number(number):
-    formatted_number = locale.format("%0.4f", number, grouping=True)
-    return formatted_number.replace(locale.localeconv()['decimal_point'], '.')
+impex_swiss_data = impex_swiss_data[['Trade Partner', 'Import Quantity (kg)', 'Export Quantity (kg)', 'Net (kg)', 'Yield (m^2/kg)', 'Share (%)', 'Weighted (m^2/kg)', 'PDF']]
 
 # Apply the custom formatting function
 impex_swiss_data['Import Quantity (kg)'] = impex_swiss_data['Import Quantity (kg)'].apply(format_swiss_number)
 impex_swiss_data['Export Quantity (kg)'] = impex_swiss_data['Export Quantity (kg)'].apply(format_swiss_number)
 impex_swiss_data['Net (kg)'] = impex_swiss_data['Net (kg)'].apply(format_swiss_number)
-impex_swiss_data['Yield (m^2/kg)'] = impex_swiss_data['Yield (m^2/kg)'].apply(format_swiss_number)
+impex_swiss_data['Yield (m^2/kg)'] = impex_swiss_data['Yield (m^2/kg)'].apply(format_swiss_number_sci)
 impex_swiss_data['Share (%)'] = impex_swiss_data['Share (%)'].apply(format_swiss_number)
-impex_swiss_data['Weighted (m^2/kg)'] = impex_swiss_data['Weighted (m^2/kg)'].apply(format_swiss_number)
+impex_swiss_data['Weighted (m^2/kg)'] = impex_swiss_data['Weighted (m^2/kg)'].apply(format_swiss_number_sci)
+impex_swiss_data['PDF'] = impex_swiss_data['PDF'].apply(format_swiss_number_sci)
 
 # Show the result
 st.subheader(f"Biodiversity Calculation (Swiss Trade) for {selected_crop.title()}")
